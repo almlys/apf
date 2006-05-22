6691cf63b817e2da6879eda75447d9f4e31b7038
alberto <alberto@ca6e00cd-310b-0410-aa3e-a0051051f01d>
2006-05-22 15:14:23 +0000

    add template
    git-svn-id: http://172.26.0.2/svn/apf/apf/trunk@32 ca6e00cd-310b-0410-aa3e-a0051051f01d

---
+++ lib/template.php
@@ -0,0 +1,9 @@
+<?php
+/*
+  Copyright (c) 2005-2006 Alberto Montañola Lacort.
+  Licensed under the GNU GPL. For full terms see the file COPYING.
+
+  Id: $Id: editpage.php 31 2006-05-22 15:09:55Z alberto $
+*/
+
+?>
