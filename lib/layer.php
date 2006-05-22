12ff97a8ba8c8beedd9fa6adfbbee9058bba0c88
alberto <alberto@ca6e00cd-310b-0410-aa3e-a0051051f01d>
2006-05-22 16:07:52 +0000

    WIP
    git-svn-id: http://172.26.0.2/svn/apf/apf/trunk@34 ca6e00cd-310b-0410-aa3e-a0051051f01d

---
+++ lib/layer.php
@@ -0,0 +1,9 @@
+<?php
+/*
+  Copyright (c) 2005-2006 Alberto Montañola Lacort.
+  Licensed under the GNU GPL. For full terms see the file COPYING.
+
+  Id: $Id$
+*/
+
+?>
---
+++ test.php
@@ -0,0 +1,23 @@
+<?php
+/*
+  Copyright (c) 2005-2006 Alberto Montañola Lacort.
+  Licensed under the GNU GPL. For full terms see the file COPYING.
+
+  Id: $Id$
+*/
+
+?>
+<html>
+<head><TITLE>test</TITLE>
+</head>
+<BODY>
+
+<div id="test">
+<fieldset>
+<legend>Upload</legend>
+<iframe src="filesystem.php">
+</iframe>
+</div>
+
+</BODY>
+</html>
