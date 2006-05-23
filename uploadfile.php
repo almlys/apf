694d05051807e05a4d1975c89a0d7894808af9c8
alberto <alberto@ca6e00cd-310b-0410-aa3e-a0051051f01d>
2006-05-23 14:55:51 +0000

    WIP
    git-svn-id: http://172.26.0.2/svn/apf/apf/trunk@36 ca6e00cd-310b-0410-aa3e-a0051051f01d

---
+++ uploadfile.php
@@ -0,0 +1,19 @@
+<?php
+/*
+  Copyright (c) 2005-2006 Alberto Montañola Lacort.
+  Licensed under the GNU GPL. For full terms see the file COPYING.
+
+  Id: $Id$
+*/
+
+//Cargar la configuración de la aplicación
+require_once(dirname(__FILE__) . "/DefaultConfig.php");
+
+//phpinfo();
+
+
+
+
+
+
+?>
